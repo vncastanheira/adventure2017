5dd7818969925faba6e0ee6c73e06b9369dbeb19
Vinícius Naves <vncastanheira@gmail.com>
2017-05-11 01:15:14 -0300

    Starting Subconscious

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,12 +20,22 @@ public class GameManager : vnc.Utilities.SingletonMonoBehaviour<GameManager>
 
     [HideInInspector] public static bool FirstPersonMode = true;
 
+    [Header("Death")]
+    public Transform RespawnPoint;
+
+    [Header("Moods")]
+    public Color HouseMood;
+    public Color SubconsciousMood;
+    public float colorTransition;
+    Coroutine transitionRoutine;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             FirstPersonMode = !FirstPersonMode;
     }
 
+    #region First Person Mode
     public void EnterFirstPerson()
     {
         FirstPersonMode = true;
@@ -35,4 +45,48 @@ public class GameManager : vnc.Utilities.SingletonMonoBehaviour<GameManager>
     {
         FirstPersonMode = false;
     }
+
+    #endregion
+
+    #region Ambient Color
+    public void SetHouseMood()
+    {
+        if (transitionRoutine != null)
+            StopCoroutine(transitionRoutine);
+        transitionRoutine = StartCoroutine(MoodTransition(HouseMood));
+    }
+
+    public void SetSubconsciousMood()
+    {
+        if(transitionRoutine != null)
+            StopCoroutine(transitionRoutine);
+        transitionRoutine = StartCoroutine(MoodTransition(SubconsciousMood));
+    }
+
+    IEnumerator MoodTransition(Color newMood)
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            var r = Mathf.Lerp(RenderSettings.ambientSkyColor.r, newMood.r, colorTransition);
+            var g = Mathf.Lerp(RenderSettings.ambientSkyColor.g, newMood.g, colorTransition);
+            var b = Mathf.Lerp(RenderSettings.ambientSkyColor.b, newMood.b, colorTransition);
+            RenderSettings.ambientSkyColor = new Color(r, g, b);
+            yield return null;
+        }
+    }
+    #endregion
+
+    #region Death
+    public void Respawn()
+    {
+        var player = FindObjectOfType<FirstPerson>();
+        player.transform.position = RespawnPoint.position;
+    }
+#endregion
+}
+
+public enum Mood
+{
+    House,
+    Subsconscious
 }
---
+++ Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour {
+
+    public UnityEvent OnTriggerEnterEvent;
+    public UnityEvent OnTriggerExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTriggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnTriggerExitEvent.Invoke();
+    }
+
+}
